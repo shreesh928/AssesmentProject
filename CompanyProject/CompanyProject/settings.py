1e17f660f46adbfb492deea95005359869b4fa1c
shreesh928 <72061686+shreesh928@users.noreply.github.com>
2024-12-24 00:02:01 +0530

    Update stiings

--- CompanyProject/CompanyProject/settings.py
+++ CompanyProject/CompanyProject/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'CompanyProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'restapi',   # Replace with your database name
+        'USER': 'root',        # Replace with your MySQL username
+        'PASSWORD': 'root',    # Replace with your MySQL password
+        'HOST': 'localhost',            # Replace with your MySQL host (e.g., 127.0.0.1)
+        'PORT': '3306',                 # Replace with your MySQL port (default is 3306)
     }
 }
 
